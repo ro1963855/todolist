3d9cefdcc565286e412fbd2c1c7241ee8a8ff6c5
Dandy.Chen <Dandy.Chen@advantech.com.tw>
2019-04-18 14:40:43 +0800

    for Heroku deploy

--- config/index.js
+++ config/index.js
@@ -45,7 +45,7 @@ module.exports = {
     proxyTable: {
       // proxy all requests starting with /api to jsonplaceholder
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'http://localhost:3001/',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
